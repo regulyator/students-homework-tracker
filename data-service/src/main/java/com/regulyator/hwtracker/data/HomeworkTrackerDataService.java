acc358b44e32dfa61cefc9159fbbd2ea65f65850
regulyator <8vWvNj@%B%pRG&GJ@eKR>
2021-08-01 01:00:02 +1000

    WIP data service added swagger

--- data-service/src/main/java/com/regulyator/hwtracker/data/HomeworkTrackerDataService.java
+++ data-service/src/main/java/com/regulyator/hwtracker/data/HomeworkTrackerDataService.java
@@ -2,10 +2,8 @@ package com.regulyator.hwtracker.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class HomeworkTrackerDataService {
 
     public static void main(String[] args) {
